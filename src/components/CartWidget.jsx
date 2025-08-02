e7da478d79a4315b2207e0eadfddc59547baf634
Lucía Palermo <lulypalermo@gmail.com>
2025-08-02 12:35:59 -0300

    Estilos en los primeros componentes

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,11 @@
+import { HiOutlineShoppingBag } from "react-icons/hi2";
 
 export const CartWidget = () => {
     return (
 
         <div id="cart-widget">
-            <img src="../shopBag.png" alt="Shop bag" />
+            {/* <img src="../shopBag.png" alt="Shop bag" /> */}
+            <HiOutlineShoppingBag />
             <span className="cart-badge">10</span>
         </div>
     )
